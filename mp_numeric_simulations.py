15268a18032ec703acb1d8f1a47f742c4974b677
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-03 19:47:11 +0100

    commenting

--- mp_numeric_simulations.py
+++ mp_numeric_simulations.py
@@ -1,12 +1,17 @@
-# to install mpmath do
-# Follow instructions in http://mpmath.org/doc/current/setup.html#download-and-installation
-# 1) download from https://pypi.python.org/packages/7a/05/b3d1472885d8dc0606936ea5da0ccb1b4785682e78ab15e34ada24aea8d5/mpmath-1.0.0.tar.gz
-# 2) extraxt the contents from the downloaded package
-# 3) Open terminal and navigate to the directory with the contents
-# 3)    tap python setup.py install
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 28 10:55:45 2017
+
+@author: eduai_000
+"""
+
+# Resources
 from mpmath import *
 import matplotlib.pyplot as plt
+# mpmath is used for bigger precision
+# To install mpmath type "pip install mpmath" on the terminal
 
+# Sets number of decimal places
 mp.dps = 33
 mp.pretty = True
 
@@ -21,7 +26,6 @@ dfAval = [fsub(fdiv(fsub(sin(fadd(x0, d)), sin(x0)), d), trueVal) for d in delta
 
 logCentre = [float(log(abs(d), 10)) for d in dfCentre]
 logAval = [float(log(abs(d), 10)) for d in dfAval]
-
 
 dfComplex = [fsub(fdiv(im(sin(mpc(x0, d))), d), mpf(trueVal)) for d in delta]
 logComplex = [float(log(d, 10)) for d in dfComplex]
