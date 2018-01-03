deb0216bab79ea2ec3af5edea2387e9383add697
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-03 19:03:28 +0100

    plotting

--- mp_numeric_simulations.py
+++ mp_numeric_simulations.py
@@ -1,34 +1,33 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy as sps
 
-#to install mpmath do
-#Follow instructions in http://mpmath.org/doc/current/setup.html#download-and-installation
-#1) download from https://pypi.python.org/packages/7a/05/b3d1472885d8dc0606936ea5da0ccb1b4785682e78ab15e34ada24aea8d5/mpmath-1.0.0.tar.gz
-#2) extraxt the contents from the downloaded package
-#3) Open terminal and navigate to the directory with the contents
-#3)	tap python setup.py install
+
+# to install mpmath do
+# Follow instructions in http://mpmath.org/doc/current/setup.html#download-and-installation
+# 1) download from https://pypi.python.org/packages/7a/05/b3d1472885d8dc0606936ea5da0ccb1b4785682e78ab15e34ada24aea8d5/mpmath-1.0.0.tar.gz
+# 2) extraxt the contents from the downloaded package
+# 3) Open terminal and navigate to the directory with the contents
+# 3)    tap python setup.py install
 from mpmath import *
-
-
-deepDel = 16
-delta = [(0.1 ** i) for i in range(deepDel)]
-
-x0 = 1.5
-trueVal = np.cos(x0)
-
-dfCentre = [(np.sin(x0+d) - np.sin(x0-d))/(2*d) for d in delta] - trueVal
-dfAval = [(np.sin(x0+d) - np.sin(x0))/d for d in delta] - trueVal 
-
-logCentre = np.log10(np.abs(dfCentre))
-logAval = np.log10(np.abs(dfAval))
+import matplotlib.pyplot as plt
 
 mp.dps = 33
 mp.pretty = True
+
+deepDel = 16
+delta = [power(mpf('0.1'), i) for i in range(deepDel)]
+
 x0 = mpf('1.5')
 trueVal = cos(x0)
-dfComplex = [fsub( fdiv( im( sin( mpc(x0, d) ) ), d ), mpf(trueVal)) for d in delta]
+
+dfCentre = [fsub(fdiv(fsub(sin(fadd(x0, d)), sin(fsub(x0, d))), (2 * d)), trueVal) for d in delta]
+dfAval = [fsub(fdiv(fsub(sin(fadd(x0, d)), sin(x0)), d), trueVal) for d in delta]
+
+logCentre = [float(log(abs(d), 10)) for d in dfCentre]
+logAval = [float(log(abs(d), 10)) for d in dfAval]
+
+
+dfComplex = [fsub(fdiv(im(sin(mpc(x0, d))), d), mpf(trueVal)) for d in delta]
 logComplex = [float(log(d, 10)) for d in dfComplex]
 
-#this should work
-plt.plot(logCentre, logAval, logComplex, range(deepDel))
+# this should work
+plt.plot(range(deepDel), logCentre, 'r--', logAval, 'bs', logComplex, 'g^')
+plt.show()
--- numeric_simullations.py
+++ numeric_simullations.py
@@ -7,7 +7,6 @@ Created on Thu Dec 28 10:55:45 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sps
 
 
 deepDel = 16
@@ -16,17 +15,17 @@ delta = [(0.1 ** i) for i in range(deepDel)]
 x0 = 1.5
 trueVal = np.cos(x0)
 
-dfCentre = [(np.sin(x0+d) - np.sin(x0-d))/(2*d) for d in delta] - trueVal
-dfAval = [(np.sin(x0+d) - np.sin(x0))/d for d in delta] - trueVal 
+dfCentre = [(np.sin(x0 + d) - np.sin(x0 - d)) / (2 * d) for d in delta] - trueVal
+dfAval = [(np.sin(x0 + d) - np.sin(x0)) / d for d in delta] - trueVal
 
 logCentre = np.log10(np.abs(dfCentre))
 logAval = np.log10(np.abs(dfAval))
 
-dfComplex = [np.imag(np.sin(x0+d*1j))/d for d in delta] - trueVal
+dfComplex = [np.imag(np.sin(x0 + d * 1j)) / d for d in delta] - trueVal
 logComplex = np.log(np.abs(dfComplex))
 
 print(logCentre)
 print(logAval)
 print(logComplex)
-plt.plot(logCentre, logAval, logComplex, range(deepDel))
-
+plt.plot(range(deepDel), logCentre, 'r--', logAval, 'bs', logComplex, 'g^')
+plt.show()
