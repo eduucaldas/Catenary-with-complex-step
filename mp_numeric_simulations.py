6e1834973e9426273849889fb1f05bf6cc2b3146
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-03 19:26:11 +0100

    .

--- mp_numeric_simulations.py
+++ mp_numeric_simulations.py
@@ -1,5 +1,3 @@
-
-
 # to install mpmath do
 # Follow instructions in http://mpmath.org/doc/current/setup.html#download-and-installation
 # 1) download from https://pypi.python.org/packages/7a/05/b3d1472885d8dc0606936ea5da0ccb1b4785682e78ab15e34ada24aea8d5/mpmath-1.0.0.tar.gz
@@ -28,6 +26,5 @@ logAval = [float(log(abs(d), 10)) for d in dfAval]
 dfComplex = [fsub(fdiv(im(sin(mpc(x0, d))), d), mpf(trueVal)) for d in delta]
 logComplex = [float(log(d, 10)) for d in dfComplex]
 
-# this should work
 plt.plot(range(deepDel), logCentre, 'r--', logAval, 'bs', logComplex, 'g^')
 plt.show()
